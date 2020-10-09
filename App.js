75520d182b6b61957c3c6c356776c56906ea1a11
PedroAugustto <augusttochaves@hotmail.com>
2020-10-09 20:30:48 -0300

    Projetos adicionado

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import Routes from './routes'
+
+import { UsuarioProvider } from './contexts/user'
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <UsuarioProvider>
+        <Routes />
+      </UsuarioProvider>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
